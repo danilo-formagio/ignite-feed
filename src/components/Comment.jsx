c1825ccd5063394b24f3e41947c00ba228f9211c
Danilo Formagio <daniloformagio@gmail.com>
2024-05-01 14:05:39 -0300

    Create Comment Component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,41 @@
+import { ThumbsUp, Trash } from "@phosphor-icons/react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img
+        className={styles.avatar}
+        src="https://avatars.githubusercontent.com/u/5075904?v=4"
+      />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Danilo Formagio</strong>
+              <time
+                title="29 de abril às 20:00h"
+                dateTime="2024-04-29 20:00:00"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
