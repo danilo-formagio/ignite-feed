defefe5647ccc0b471183653f3f5e617c80a5793
Danilo Formagio <daniloformagio@gmail.com>
2024-05-10 20:56:02 -0300

    Validate form

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -32,16 +32,23 @@ export function Post({ author, publishedAt, content }) {
   }
 
   function handleNewCommentChange() {
+    event.target.setCustomValidity("");
     setNewCommentText(event.target.value);
   }
 
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity("Esse campo é obrigatório!");
+  }
+
   function deleteComment(commentToDelete) {
-    const commentsWithoutDeleteOne = comments.filter(comment => {
+    const commentsWithoutDeleteOne = comments.filter((comment) => {
       return comment !== commentToDelete;
     });
 
     setComments(commentsWithoutDeleteOne);
   }
+
+  const isNewCommentEmpty = newCommentText.length === 0;
 
   return (
     <article className={styles.post}>
@@ -81,10 +88,14 @@ export function Post({ author, publishedAt, content }) {
           placeholder="Deixe um comentário"
           value={newCommentText}
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
 
         <footer>
-          <button type="submit">Publicar</button>
+          <button type="submit" disabled={isNewCommentEmpty}>
+            Publicar
+          </button>
         </footer>
       </form>
 
